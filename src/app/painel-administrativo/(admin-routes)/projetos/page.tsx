62dfa1c801aa505b5c74425253462c511b131747
P4vanzinho <thiago772000@gmail.com>
2024-03-20 14:05:44 -0300

    feat: create projetos page

---
+++ src/app/painel-administrativo/(admin-routes)/projetos/page.tsx
@@ -0,0 +1,12 @@
+import { Container } from "./styles";
+
+export default function Projetos() {
+  return (
+    <Container>
+      <h1>
+        estamos na pagina PROJETOS, aqui podemos visualizar os cards dos
+        projetos ja criados.
+      </h1>
+    </Container>
+  );
+}
