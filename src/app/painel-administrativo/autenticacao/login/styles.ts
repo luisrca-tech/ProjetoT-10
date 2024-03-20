aa70007cc5ac79b0341a9558def0b8bc4744b3b7
P4vanzinho <thiago772000@gmail.com>
2024-03-20 14:05:32 -0300

    feat : create login page

---
+++ src/app/painel-administrativo/autenticacao/login/styles.ts
@@ -0,0 +1,16 @@
+import { styled } from "@linaria/react";
+
+export const Container = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+
+  h1 {
+    font-size: 1rem;
+  }
+
+  button {
+    width: 50px;
+    height: 50px;
+  }
+`;
