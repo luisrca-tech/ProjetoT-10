3f879da605d0604fd8205db0e8c78dd5afdad947
P4vanzinho <thiago772000@gmail.com>
2024-03-20 09:02:05 -0300

    feat : create projeto page and her styles

---
+++ src/app/projeto/page.tsx
@@ -0,0 +1,12 @@
+import { Container } from "./styles";
+
+export default function Projeto() {
+  return (
+    <Container>
+      <h1>
+        estamos na pagina PROJETO, aqui os projetos poderam ser criados ou
+        editados.
+      </h1>
+    </Container>
+  );
+}
---
+++ src/app/projeto/styles.ts
@@ -0,0 +1,11 @@
+import { styled } from "@linaria/react";
+
+export const Container = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+
+  h1 {
+    font-size: 1rem;
+  }
+`;
