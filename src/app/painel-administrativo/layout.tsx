055705eebe6bacea4bb56503710ac150665fc40d
P4vanzinho <162153619+P4vanzinho@users.noreply.github.com>
2024-03-21 16:17:36 -0300

    Revert "Feature/86a2pkdue"

--- src/app/painel-administrativo/layout.tsx
+++ src/app/painel-administrativo/layout.tsx
@@ -1,7 +1,7 @@
 import { ReactNode } from "react";
 
 import { styled } from "@linaria/react";
-import Header from "../components/Header";
+import Header from "../components/header";
 
 const Container = styled.div`
   width: 100%;
