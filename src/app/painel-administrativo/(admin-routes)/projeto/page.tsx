a07b45778dd38fd57f509c8ff43cbe7e13993db7
Luis Felipe <luisrochacruzalves@gmail.com>
2024-03-25 19:57:13 -0300

    feat: create parcial structure for project page

--- src/app/painel-administrativo/(admin-routes)/projeto/page.tsx
+++ src/app/painel-administrativo/(admin-routes)/projeto/page.tsx
@@ -1,12 +1,30 @@
-import { Container } from "./styles";
+import { SelectInputText } from "@/app/components/SelectInputText";
+import {
+  BackgroundSizeImage,
+  Container,
+  InputsDataContainer,
+  TitleProjectContainer,
+} from "./styles";
+import { RiPencilFill } from "react-icons/ri";
 
 export default function Projeto() {
   return (
     <Container>
-      <h1>
-        estamos na pagina PROJETO, aqui os projetos poderam ser criados ou
-        editados.
-      </h1>
+      <TitleProjectContainer>
+        <BackgroundSizeImage>P1</BackgroundSizeImage>
+        <strong>Projeto exemplo 1</strong>
+        <RiPencilFill />
+      </TitleProjectContainer>
+
+      <InputsDataContainer>
+        <SelectInputText
+          optionId={1}
+          placeholder="Cargo"
+          options={["Back-end SR", "Back-end PL", "Front-end SR"]}
+        />
+        <SelectInputText placeholder="Horas" optionId={2} />
+        <SelectInputText placeholder="Valor" optionId={3} />
+      </InputsDataContainer>
     </Container>
   );
 }
