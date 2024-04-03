3761cca3e175a374045653e9ee651145444682e8
Luis Felipe <luisrochacruzalves@gmail.com>
2024-04-03 17:59:38 -0300

    refactor: delete comments and imports from Select component

--- src/app/components/SelectInputWithDropdown/index.tsx
+++ src/app/components/SelectInputWithDropdown/index.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Creatable from "react-select/creatable";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
@@ -8,13 +8,14 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import ArrowRight from "../../../../public/arrowright.svg";
 import ArrowDown from "../../../../public/arrowdown.svg";
 import Image from "next/image";
-import { customStyles } from "./styles";
+import { Container, customStyles } from "./styles";
 import {
   OptionType,
   SelectInputProps,
   SelectInputTextProps,
   SelectInputTextSchema,
 } from "./SelectUtils";
+import { roboto } from "@/app/fonts";
 
 export function SelectInputWithDropdown({
   placeholder,
@@ -30,6 +31,14 @@ export function SelectInputWithDropdown({
   const { register } = useForm<SelectInputProps & SelectInputTextProps>({
     resolver: zodResolver(SelectInputTextSchema),
   });
+
+  // // Carrega as opções salvas do localStorage ao montar o componente
+  // useEffect(() => {
+  //   const savedOptions = localStorage.getItem("customOptions");
+  //   if (savedOptions) {
+  //     setCustomOptions(JSON.parse(savedOptions));
+  //   }
+  // }, []);
 
   const handleChangeMenu = (newIsOpen: boolean) => {
     setIsMenuOpen(newIsOpen);
@@ -70,11 +79,18 @@ export function SelectInputWithDropdown({
     setCustomOptions((prevOptions) => [...prevOptions, newOption]);
 
     setSelectedOptions([newOption]);
+
+    // // Salvando a nova opção em localStorage
+    // const savedOptions = localStorage.getItem("customOptions");
+    // const options = savedOptions ? JSON.parse(savedOptions) : [];
+    // const updatedOptions = [...options, newOption];
+    // localStorage.setItem("customOptions", JSON.stringify(updatedOptions));
   };
 
   return (
-    <div>
+    <Container className={roboto.className}>
       <Creatable
+        className={roboto.className}
         {...register("query")}
         value={selectedOptions}
         onChange={handleChange}
@@ -113,6 +129,6 @@ export function SelectInputWithDropdown({
         }}
         noOptionsMessage={() => "Nenhuma opção disponível"}
       />
-    </div>
+    </Container>
   );
 }
