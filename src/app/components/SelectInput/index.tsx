03a54650cfc498084db54e76ad8c5a825f0df076
Luis Felipe <luisrochacruzalves@gmail.com>
2024-04-09 10:27:40 -0300

    fix: arrange_git_flow

---
+++ src/app/components/SelectInput/index.tsx
@@ -0,0 +1,50 @@
+"use client";
+import { useRef, useState } from "react";
+import { Container } from "./styles";
+
+import { backgroundImages, position } from "polished";
+import { SelectComponent } from "../SelectComponent";
+
+interface SelectInputProps {
+  id: string;
+  onChange: (value: string) => void;
+  placeholder: string;
+}
+
+export default function SelectInput({
+  id,
+  onChange,
+  placeholder,
+}: SelectInputProps) {
+  const [value, setValue] = useState("");
+
+  const handleChange = (
+    event: React.ChangeEvent<HTMLSelectElement | HTMLInputElement>,
+  ) => {
+    if (
+      event.target instanceof HTMLSelectElement ||
+      event.target instanceof HTMLInputElement
+    ) {
+      const newValue = event.target.value;
+      setValue(newValue);
+      onChange(newValue);
+    }
+  };
+
+  return (
+    <Container>
+      <input
+        placeholder={placeholder}
+        type="text"
+        id={id}
+        value={value}
+        onChange={handleChange}
+      />
+      <SelectComponent
+        id={id}
+        value={value}
+        onChange={handleChange}
+      />
+    </Container>
+  );
+}
