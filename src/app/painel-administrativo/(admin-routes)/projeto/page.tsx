a4be04684029ff02d07e00bf51611fc5b800ab51
P4vanzinho <thiago772000@gmail.com>
2024-04-09 14:33:39 -0300

    feat : add ToggleSwitch component in page

--- src/app/painel-administrativo/(admin-routes)/projeto/page.tsx
+++ src/app/painel-administrativo/(admin-routes)/projeto/page.tsx
@@ -1,14 +1,30 @@
 "use client";
 
 import FormSelectInput from "@/app/components/FormSelectInput";
-import { Container, InputsData, InputsDataContainer } from "./styles";
+import {
+  Container,
+  InputsData,
+  InputsDataContainer,
+  SwitchContainer,
+} from "./styles";
 import { ProjectProfileHeader } from "@/app/components/ProjectProfileHeader";
 import { roboto } from "@/app/fonts";
+import { useState } from "react";
+import ToggleSwitch from "@/app/components/ToggleSwitch";
 
 export default function Projeto() {
+  const [checked, setChecked] = useState<boolean>(false);
+
+  const handleChange = () => {
+    setChecked(!checked);
+  };
   return (
     <Container className={roboto.className}>
       <ProjectProfileHeader value="Nome do Projeto..." />
+      <SwitchContainer>
+        <span>Editar datas</span>
+        <ToggleSwitch onChange={handleChange} />
+      </SwitchContainer>
       <FormSelectInput />
     </Container>
   );
