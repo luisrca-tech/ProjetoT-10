614aa072b2fa2a140a1f94d74c13941c0496c580
Luis Felipe <luisrochacruzalves@gmail.com>
2024-04-10 09:54:49 -0300

    feat: add conditional width to container box input at checked

--- src/app/components/SelectInput/styles.ts
+++ src/app/components/SelectInput/styles.ts
@@ -2,30 +2,37 @@ import { theme } from "@/app/styles/theme";
 import { styled } from "@linaria/react";
 
 type ContainerProps = {
-  hasValue: Boolean;
+  checked: boolean;
 };
 
 export const Container = styled.div<ContainerProps>`
   display: flex;
-  width: 33%;
+  width: ${props => props.checked ? 'calc(100% - 28px)' : '32%'};
   height: 2.5rem;
   align-items: center;
-  position: relative;
+`;
 
-  input {
-    position: relative;
-    display: inline-block;
-    padding-left: 0.75rem;
-    height: 100%;
-    width: 100%;
-    background-color: ${theme.COLORS.SELECT_INPUT};
-    border-radius: 60px;
-    border: none;
-    outline: none;
+type InputProps = {
+  hasValue: Boolean;
+};
 
-    &::placeholder {
-      font-size: 1rem;
-      color: ${theme.COLORS.SECONDARY_DARK};
-    }
+export const Input = styled.input<InputProps>`
+  display: inline-block;
+  padding-left: 0.75rem;
+  height: 100%;
+  width: 100%;
+  background-color: ${(props) =>
+    props.hasValue ? 'transparent' : `${theme.COLORS.SELECT_INPUT}`};
+  border-radius: 60px;
+  border: none;
+  outline: none;
+
+  &::placeholder {
+    font-size: 1rem;
+    color: ${theme.COLORS.SECONDARY_DARK};
   }
+`;
+
+export const SelectComponentContainer = styled.div`
+  position: relative;
 `;
