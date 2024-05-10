c09524a33582b8f6e7ff0c37179e9d2485b0bbd1
Luis Felipe <luisrochacruzalves@gmail.com>
2024-05-10 01:08:06 -0300

    patch: Add new styles to DateRange custom component

--- src/app/components/CustomDateRangePicker/index.tsx
+++ src/app/components/CustomDateRangePicker/index.tsx
@@ -4,17 +4,34 @@ import { DateRangePicker } from "rsuite";
 import "rsuite/DateRangePicker/styles/index.css";
 import "./DateRangePicker.css";
 import { Container } from "./styles";
+import { use, useState } from "react";
+
+import { enGB } from "date-fns/locale";
 
 export function CustomDateRangePicker() {
+  const [format, setFormat] = useState<string>(
+    `Data inicial ${"               "} | ${"             "} Data final`,
+  );
+
+  const ptBR = {
+    sunday: "Dom",
+    monday: "Seg",
+    tuesday: "Ter",
+    wednesday: "Qua",
+    thursday: "Qui",
+    friday: "Sex",
+    saturday: "Sáb",
+  };
+
   return (
     <Container>
       <DateRangePicker
-        className="rs-picker-daterange-panel"
-        placeholder={`Data inicial ${"               "} | ${"             "} Data final`}
+        placeholder={`Data inicial ${"           "} | ${"         "} Data final`}
         editable={true}
         showHeader={true}
-        format="dd-MM-yyyy"
-        style={{ minWidth: "none" }}
+        format={`${"            "}dd/MM/yyyy ${"            "}`}
+        locale={ptBR}
+        isoWeek
       />
     </Container>
   );
