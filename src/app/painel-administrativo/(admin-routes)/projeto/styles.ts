ac48bd70cc88bafc9fa722e00168fb34d3d54df1
P4vanzinho <thiago772000@gmail.com>
2024-05-16 13:08:08 -0300

    refactor : add padding in Container

--- src/app/painel-administrativo/(admin-routes)/projeto/styles.ts
+++ src/app/painel-administrativo/(admin-routes)/projeto/styles.ts
@@ -9,6 +9,7 @@ export const Container = styled.div`
   align-items: center;
   justify-content: center;
   padding-bottom: 2rem;
+  padding-top: 1rem;
 `;
 
 export const InputsDataContainer = styled.div`
