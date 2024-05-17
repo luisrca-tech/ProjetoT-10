b377a00c89321506795676e2958c817cd5166972
P4vanzinho <thiago772000@gmail.com>
2024-05-17 12:13:23 -0300

    refactor : refact layout

--- src/app/painel-administrativo/layout.tsx
+++ src/app/painel-administrativo/layout.tsx
@@ -1,4 +1,5 @@
-import { ReactNode } from "react";
+"use client";
+import { ReactNode, useState } from "react";
 
 import { styled } from "@linaria/react";
 import Header from "../components/header";
@@ -7,8 +8,6 @@ import { ProjectProfileHeader } from "../components/ProjectProfileHeader";
 const Container = styled.div`
   width: 100%;
   height: 100%;
-
-  padding-top: 12.594rem;
 `;
 
 type AuthHeaderProps = {
@@ -16,25 +15,9 @@ type AuthHeaderProps = {
 };
 
 export default function PrivateLayout({ children }: AuthHeaderProps) {
-  //Precisa de checked e isDatePickerOpen,checked, setChecked de um contexto para esse ProjectProfileHeader funcionar.
-
-  /*  
-    function ToggleDatePicker(){
-      setIsDatePickerOpen(!isDatePickerOpen);
-      //isDatePickerOpen->CONTEXTO
-    }
-
-*/
-
   return (
     <>
       <Header />
-      <ProjectProfileHeader
-      // checked={checked}  PEGAR CHECKED DO CONTEXTO.
-      // value="Nome do Projeto..."
-      // toggleDatePicker={ToggleDatePicker}
-      //ADICIONAR AS PROPRIEDADES DEPOIS QUE FIZER O CONTEXTO ( PRODUZIR FUNCAO ToggleDatePicker)
-      />
       <Container>{children}</Container>
     </>
   );
