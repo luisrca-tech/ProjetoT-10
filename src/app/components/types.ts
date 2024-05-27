a87efb3469d1b487e08026246d179b82aea2d666
P4vanzinho <thiago772000@gmail.com>
2024-05-27 10:35:16 -0300

    refactor : reduce type

--- src/app/components/types.ts
+++ src/app/components/types.ts
@@ -3,37 +3,26 @@ import { Range } from "react-date-range";
 export interface SelectableRange extends Range {
   isSelected?: boolean;
 }
+
 export interface RowsAndSelectedValueProps {
   rows: string[];
   selectedValues: { [key: string]: string };
 }
+
 export interface FormSelectInputProps {
   setStringRow: React.Dispatch<React.SetStateAction<string>>;
-
   rowCount: number;
-  setRanges: React.Dispatch<
-    React.SetStateAction<{ [key: string]: SelectableRange }>
-  >;
+  setRanges: React.Dispatch<React.SetStateAction<{ [key: string]: SelectableRange }>>;
   setRowCount: React.Dispatch<React.SetStateAction<number>>;
   checked: boolean;
   ranges: { [key: string]: SelectableRange };
-
   inputDataMenuClick: (row: string) => void;
 }
 
-export interface RowAndScrollDownContainerProps {
-  setStringRow: React.Dispatch<React.SetStateAction<string>>;
+export interface CommonProps extends FormSelectInputProps {
   row: string;
-  rowCount: number;
-  setRanges: React.Dispatch<
-    React.SetStateAction<{ [key: string]: SelectableRange }>
-  >;
-  setRowCount: React.Dispatch<React.SetStateAction<number>>;
-  checked: boolean;
-  ranges: { [key: string]: SelectableRange };
   rowsAndSelectedValues: RowsAndSelectedValueProps;
-  setRowsAndSelectedValues: React.Dispatch<
-    React.SetStateAction<RowsAndSelectedValueProps>
-  >;
-  inputDataMenuClick: (row: string) => void;
+  setRowsAndSelectedValues: React.Dispatch<React.SetStateAction<RowsAndSelectedValueProps>>;
 }
+
+export interface RowAndScrollDownContainerProps extends CommonProps {}
