fbf9e3996059699ac9f9612a877c0e1ef564401c
Luis Felipe <luisrochacruzalves@gmail.com>
2024-06-07 13:10:47 -0300

    fix: delete imports

--- src/app/painel-administrativo/(admin-routes)/projeto/page.tsx
+++ src/app/painel-administrativo/(admin-routes)/projeto/page.tsx
@@ -10,7 +10,6 @@ import {
 } from "./styles";
 import { ProjectProfileHeader } from "../../../components/surfaces/ProjectProfileHeader";
 import { roboto } from "@/app/fonts";
-import { useEffect, useState } from "react";
 import ToggleSwitch from "@/app/components/widgets/ToggleSwitch";
 import { CustomDateRangePicker } from "@/app/components/widgets/CustomDateRangePicker";
 import Header from "@/app/components/surfaces/header";
@@ -18,10 +17,6 @@ import { useAtom } from "jotai";
 import { checkedAtom } from "@/@atom/ProjectStates/checkedAtom";
 import { isDatePickerOpenAtom } from "@/@atom/ProjectStates/isDatePickerOpenAtom";
 import { stringRowAtom } from "@/@atom/ProjectStates/stringRowAtom";
-import { getCustomFields } from "@/app/services/api/customFields/getCustomFields";
-import { postTasks } from "@/app/services/api/tasks/postTask";
-import { updateTask } from "@/app/services/api/tasks/updateTask";
-import { chargeOptionsAtom } from "@/@atom/api/CustomFields/chargeOptionsAtom";
 
 export default function Projeto() {
   const [ checked, setChecked ] = useAtom(checkedAtom)
