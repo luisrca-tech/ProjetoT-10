3334bb3be6e5cec7be0b2164d98e60444973bae0
P4vanzinho <thiago772000@gmail.com>
2024-06-16 17:52:40 -0300

    feat : add header in projetos page

--- src/app/painel-administrativo/(admin-routes)/projetos/page.tsx
+++ src/app/painel-administrativo/(admin-routes)/projetos/page.tsx
@@ -1,9 +1,11 @@
 import { ProjectCard } from "@/app/components/surfaces/project-card";
 import { Container } from "./styles";
+import Header from "@/app/components/surfaces/header";
 
 export default function Projetos() {
   return (
     <Container>
+      <Header />
       <ProjectCard />
     </Container>
   );
