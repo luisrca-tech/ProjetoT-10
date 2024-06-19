90ebc7f12401d0ae403a988f90df15f0b18e81b0
P4vanzinho <thiago772000@gmail.com>
2024-06-19 14:18:07 -0300

    fix : condicional styles in ToggleSwitch

--- src/app/components/widgets/ToggleSwitch/index.tsx
+++ src/app/components/widgets/ToggleSwitch/index.tsx
@@ -1,17 +1,20 @@
 import { Container } from "./styles";
 import * as Switch from "@radix-ui/react-switch";
-
+import { checkedAtom } from "@/@atom/ProjectStates/checkedAtom";
+import { useAtom } from "jotai";
 type ButtonRegistrationType = {
   onChange: () => void;
 };
 
 export default function ToogleSwitch({ onChange }: ButtonRegistrationType) {
+  const [checked] = useAtom(checkedAtom);
   return (
     <Container>
       <Switch.Root
         className="SwitchRoot"
         id="airplane-mode"
         onCheckedChange={onChange}
+        checked={checked}
       >
         <Switch.Thumb className="SwitchThumb" />
       </Switch.Root>
