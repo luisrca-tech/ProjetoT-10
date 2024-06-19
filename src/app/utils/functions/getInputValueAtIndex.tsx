9b139246e0fcb2569cd03c1ae8d466cd790e335e
P4vanzinho <thiago772000@gmail.com>
2024-06-19 14:12:25 -0300

    feat : create useGetInputValueAtIndex util function

---
+++ src/app/utils/functions/getInputValueAtIndex.tsx
@@ -0,0 +1,24 @@
+import { rowsAndSelectedValuesAtom } from "@/@atom/ProjectStates/rowsAndSelectedValuesAtom";
+import { projectSelectedValuePropAtom } from "@/@atom/ProjectStates/projectSelectedValue";
+import { useAtom } from "jotai";
+
+export function useGetInputValueAtIndex(
+  linePrefix?: string,
+  row?: string,
+  inProfileHeader?: boolean,
+) {
+  const [rowsAndSelectedValues] = useAtom(rowsAndSelectedValuesAtom);
+  const [projectSelectedValue] = useAtom(projectSelectedValuePropAtom);
+
+  if (inProfileHeader) {
+    const { selectedValue } = projectSelectedValue;
+    const inputValue = selectedValue[`${row}-text`];
+
+    return inputValue;
+  }
+
+  const inputValue =
+    rowsAndSelectedValues.selectedValues[`${linePrefix}${row}-text`];
+
+  return inputValue;
+}
