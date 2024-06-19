efad0c90bb6941046db66f3371b1caf5e51d0060
P4vanzinho <thiago772000@gmail.com>
2024-06-19 14:11:43 -0300

    feat : create useIsSelectOpen util function

---
+++ src/app/utils/functions/isSelectOpen.tsx
@@ -0,0 +1,7 @@
+import { useAtom } from "jotai";
+import { selectedItemIndexAtom } from "@/@atom/ProjectStates/selectedItemIndexAtom";
+
+export function useIsSelectOpen(index: string) {
+  const [selectedItemIndex] = useAtom(selectedItemIndexAtom);
+  return selectedItemIndex === index;
+}
