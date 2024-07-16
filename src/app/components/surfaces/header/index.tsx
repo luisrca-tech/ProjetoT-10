91ff358424a98acaf44cfb794ea61ad62872cd01
P4vanzinho <thiago772000@gmail.com>
2024-07-16 10:02:43 -0300

    refactor :  remove getCustomField from the component

--- src/app/components/surfaces/header/index.tsx
+++ src/app/components/surfaces/header/index.tsx
@@ -22,94 +22,22 @@ import { usePathname } from "next/navigation";
 import { RiCheckFill } from "react-icons/ri";
 import { postTasks } from "@/app/services/api/tasks/postTask";
 import { toast } from "react-toastify";
-import { getCustomFields } from "@/app/services/api/customFields/getCustomFields";
 import { useAtom } from "jotai";
-import { chargeOptionsAtom } from "@/@atom/api/CustomFields/chargeOptionsAtom";
-import { projectOptionsAtom } from "@/@atom/api/CustomFields/projectFieldAtom";
 import { projectSelectedValuePropAtom } from "@/@atom/ProjectStates/projectSelectedValue";
-import { loading } from "@/@atom/LoadingState/loadingAtom";
+import { loadingAtom } from "@/@atom/LoadingState/loadingAtom";
 import { rowsAndSelectedValuesAtom } from "@/@atom/ProjectStates/rowsAndSelectedValuesAtom";
 import { rangesAtom } from "@/@atom/ProjectStates/rangesAtom";
-
-type FieldsIdType = {
-  chargeFieldId: string;
-  projectFieldId: string;
-  valueFieldId: string;
-  hoursPerMonthCustomFieldId: string;
-};
+import { fieldsIdsAtom } from "@/@atom/api/CustomFields/fieldsIds";
 
 export default function Header() {
-  const [, setProjectOptions] = useAtom(projectOptionsAtom);
-  const [, setChargeOptions] = useAtom(chargeOptionsAtom);
   const [ranges] = useAtom(rangesAtom);
-  const [, setLoading] = useAtom(loading);
+  const [loading, setLoading] = useAtom(loadingAtom);
   const [rowsAndSelectedValues] = useAtom(rowsAndSelectedValuesAtom);
   const [projectSelectedValue] = useAtom(projectSelectedValuePropAtom);
   const [showModal, setShowModal] = useState<boolean>(false);
-  const [, setGetCustomFieldsResponse] = useState([]);
-  const listId = "901303987731"; //mocado.
-  const [fieldsIds, setFieldsIds] = useState<FieldsIdType>({
-    chargeFieldId: "",
-    projectFieldId: "",
-    valueFieldId: "",
-    hoursPerMonthCustomFieldId: "",
-  });
+  const [fieldsIds] = useAtom(fieldsIdsAtom);
   const router = useRouter();
   const currentPath = usePathname();
-
-  useEffect(() => {
-    async function customFieldsGetRequest() {
-      setLoading(true);
-
-      const getCustomFieldResp = await getCustomFields(listId);
-
-      if (!getCustomFieldResp) {
-        toast.error("Nao foi possível acessar customFields deste listId !");
-      }
-
-      setGetCustomFieldsResponse(getCustomFieldResp);
-      const chargeCustomField = getCustomFieldResp.find(
-        (field: { name: string }) => field.name === "PixelCraft_cargos",
-      );
-
-      const projectCustomField = getCustomFieldResp.find(
-        (field: { name: string }) => field.name === "PixelCraft_projeto",
-      );
-
-      const hoursPerMonthCustomField = getCustomFieldResp.find(
-        (field: { name: string }) => field.name === "PixelCraft_Horas_Mes",
-      );
-
-      const ValueCustomField = getCustomFieldResp.find(
-        (field: { name: string }) => field.name === "PixelCraft_Valor",
-      );
-
-      const valueFieldId = ValueCustomField.id;
-      const hoursPerMonthCustomFieldId = hoursPerMonthCustomField.id;
-
-      const chargeFieldId = chargeCustomField.id;
-      const projectFieldId = projectCustomField.id;
-
-      const fieldsIds = {
-        chargeFieldId,
-        projectFieldId,
-        valueFieldId,
-        hoursPerMonthCustomFieldId,
-      };
-      setFieldsIds(fieldsIds);
-
-      const projectOptions = projectCustomField.type_config.options;
-      setProjectOptions(projectOptions);
-
-      const chargeOptions = chargeCustomField.type_config.options;
-      setChargeOptions(chargeOptions);
-
-      setLoading(false);
-    }
-
-    customFieldsGetRequest();
-    toast.success("CustomFields da lista acessados !");
-  }, [setChargeOptions, setLoading, setProjectOptions]);
 
   async function taskPostRequest() {
     try {
