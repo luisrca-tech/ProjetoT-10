2c7e34d4f062f69967f841728a012e07ff1abf48
P4vanzinho <thiago772000@gmail.com>
2024-07-17 21:32:49 -0300

    feat : make getTasks and create filters

--- src/app/painel-administrativo/(admin-routes)/projeto/page.tsx
+++ src/app/painel-administrativo/(admin-routes)/projeto/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useSearchParams } from "next/navigation";
 import FormSelectInput from "@/app/components/forms/FormSelectInput";
 import {
   CloseCalendarContainer,
@@ -19,6 +20,11 @@ import { isDatePickerOpenAtom } from "@/@atom/ProjectStates/isDatePickerOpenAtom
 import { stringRowAtom } from "@/@atom/ProjectStates/stringRowAtom";
 import useClickUpFetch from "@/app/hooks/useClickUpFetch";
 import { EndPointClickUpApiEnum } from "@/clickUpApi/EndPointClickUpApiEnum";
+import { Task } from "@/app/types/clickUpApi";
+import { rowCountAtom } from "@/@atom/ProjectStates/rowCountAtom";
+import { rowsAndSelectedValuesAtom } from "@/@atom/ProjectStates/rowsAndSelectedValuesAtom";
+import { useEffect, useCallback } from "react";
+import { rangesAtom } from "@/@atom/ProjectStates/rangesAtom";
 
 type FieldsIdType = {
   chargeFieldId: string;
@@ -31,6 +37,13 @@ export default function Projeto() {
   const [checked, setChecked] = useAtom(checkedAtom);
   const [isDatePickerOpen, setIsDatePickerOpen] = useAtom(isDatePickerOpenAtom);
   const [, setStringRow] = useAtom(stringRowAtom);
+  const [rowCount, setRowCount] = useAtom(rowCountAtom);
+  const searchParams = useSearchParams();
+  const projectId = searchParams.get("projectId");
+  const [rowsAndSelectedValues, setRowsAndSelectedValues] = useAtom(
+    rowsAndSelectedValuesAtom,
+  );
+  const [ranges, setRanges] = useAtom(rangesAtom);
 
   function openDatePicker() {
     setIsDatePickerOpen(true);
@@ -49,9 +62,113 @@ export default function Projeto() {
     setIsDatePickerOpen(false);
   };
 
-  const { isFetchAllCustomFields } = useClickUpFetch(
+  const { projectOptions, isFetchAllCustomFields } = useClickUpFetch(
     EndPointClickUpApiEnum.FIELD,
   );
+
+  const { data: getTaskResp } = useClickUpFetch<Task[]>(
+    EndPointClickUpApiEnum.TASK,
+  );
+
+  const updateRowsAndSelectedValues = useCallback(
+    (tasksForProject: Task[]) => {
+      let count = 0;
+      tasksForProject.forEach((task, index) => {
+        count = index;
+
+        const cargoField = task.custom_fields.find(
+          (field) => field.name === "PixelCraft_cargos",
+        );
+        const horasField = task.custom_fields.find(
+          (field) => field.name === "PixelCraft_Horas_Mes",
+        );
+        const valorField = task.custom_fields.find(
+          (field) => field.name === "PixelCraft_Valor",
+        );
+        const taskStartDate = task.start_date
+          ? new Date(parseInt(task.start_date))
+          : new Date();
+        const taskDueDate = task.due_date
+          ? new Date(parseInt(task.due_date))
+          : new Date();
+
+        console.log(taskStartDate, `taskStartDate`);
+        console.log(taskDueDate, `taskDueDate`);
+
+        let cargoName = "";
+        if (cargoField && Array.isArray(cargoField.type_config?.options)) {
+          const option = cargoField.type_config.options.find(
+            (opt) => opt.id === cargoField.value,
+          );
+          cargoName = option ? option.name : "";
+        }
+
+        const cargoValue = cargoField ? `${cargoField.value}` : "";
+        const horasValue = horasField ? `${horasField.value}` : "";
+        const valorValue = valorField ? `${valorField.value}` : "";
+
+        console.log(cargoName, `cargoName`);
+        console.log(cargoValue, `cargoValue`);
+        console.log(horasValue, `horasValue`);
+        console.log(valorValue, `valorValue`);
+
+        setRowsAndSelectedValues((prevState) => ({
+          ...prevState,
+          rows: [...prevState.rows, `row-${count}`],
+          selectedValues: {
+            ...prevState.selectedValues,
+            [`firstTextValuerow-${count}-text`]: cargoName,
+            [`firstTextValuerow-${count}-option`]: cargoValue,
+            [`secondTextValuerow-${count}-text`]: horasValue,
+            [`thirdTextValuerow-${count}-text`]: valorValue,
+          },
+        }));
+
+        setRanges((prevState) => ({
+          ...prevState,
+          [`row-${count}`]: {
+            endDate: taskDueDate,
+            isSelected: true,
+            key: `selection-row-${count}`,
+            startDate: taskStartDate,
+          },
+        }));
+      });
+
+      const nextRow = `row-${count + 1}`;
+      setRowsAndSelectedValues((prevState) => ({
+        ...prevState,
+        rows: [...prevState.rows, nextRow],
+      }));
+      setRanges((prevState) => ({
+        ...prevState,
+        [nextRow]: {
+          endDate: undefined,
+          isSelected: false,
+          key: `selection-row-${nextRow}`,
+          startDate: undefined,
+        },
+      }));
+    },
+    [setRanges, setRowsAndSelectedValues],
+  );
+
+  useEffect(() => {
+    if (projectId && getTaskResp) {
+      const tasksForProject = getTaskResp.filter((task) =>
+        task.custom_fields.some((field) => {
+          if (Array.isArray(field.value)) {
+            return field.value.includes(projectId);
+          }
+          return false;
+        }),
+      );
+      console.log(tasksForProject, "tasksForProject");
+      if (tasksForProject.length > 0) {
+        updateRowsAndSelectedValues(tasksForProject);
+      }
+    }
+  }, [projectId, getTaskResp, updateRowsAndSelectedValues]);
 
   return (
     <Container className={roboto.className}>
