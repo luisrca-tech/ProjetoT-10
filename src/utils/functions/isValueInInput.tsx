a6f212f0d0032cd6f9e16abf11ba2a1ffc4c90cf
P4vanzinho <thiago772000@gmail.com>
2024-07-24 14:25:11 -0300

    refactor : remove desnecessary param

--- src/utils/functions/isValueInInput.tsx
+++ src/utils/functions/isValueInInput.tsx
@@ -1,11 +1,7 @@
 import { useAtom } from "jotai";
 import { rowsAndSelectedValuesAtom } from "~/@atom/ProjectStates/rowsAndSelectedValuesAtom";
 
-export function useIsValueInInput(
-  row: string,
-  inputName: string,
-  inProfileHeader?: boolean,
-) {
+export function useIsValueInInput(row: string, inputName: string) {
   const [rowsAndSelectedValues] = useAtom(rowsAndSelectedValuesAtom);
   const { selectedValues } = rowsAndSelectedValues;
   const textValue = selectedValues[`${inputName}${row}-text`];
