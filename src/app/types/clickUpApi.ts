6d64216596a7dbdb16d9f5a285ecc493bec19a37
P4vanzinho <thiago772000@gmail.com>
2024-07-24 14:21:02 -0300

    feat : add fields types

--- src/app/types/clickUpApi.ts
+++ src/app/types/clickUpApi.ts
@@ -28,6 +28,47 @@ export type CustomField = {
   value: string[];
 };
 
+export type ProjectOptionType = {
+  id: string;
+  label: string;
+  color: null;
+};
+
+export type ProjectFieldType = {
+  id: string;
+  name?: string;
+  type?: string;
+  type_config?: {
+    new_drop_down: boolean;
+    options: ProjectOptionType;
+  };
+  date_created?: string;
+  hide_from_guests?: boolean;
+  required?: boolean;
+  value: [string];
+};
+
+export type ChargeOptionType = {
+  id?: string;
+  name: string;
+  color?: string | null;
+  orderindex?: number;
+  value: string;
+};
+
+export type ChargeFieldType = {
+  id?: string;
+  name?: string;
+  type?: string;
+  type_config: {
+    new_drop_down: boolean;
+    options: ChargeOptionType;
+  };
+  date_created?: string;
+  hide_from_guests?: boolean;
+  required?: boolean;
+};
+
 interface Folder {
   id: string;
   name: string;
