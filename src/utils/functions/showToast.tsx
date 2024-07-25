a2ea23fcaddf64f6d8035e7055fe975ee80dcf1d
P4vanzinho <thiago772000@gmail.com>
2024-07-25 09:05:51 -0300

    feat: create showToast component

---
+++ src/utils/functions/showToast.tsx
@@ -0,0 +1,17 @@
+import { toast } from "sonner";
+import ToastAlert from "~/app/components/widgets/ToastAlert";
+
+export function showToast(
+  status: "error" | "success" | "warning",
+  message: string,
+  justifyMessage?: string
+) {
+  toast(
+    <ToastAlert
+      status={status}
+      message={message}
+      justifyMessage={justifyMessage}
+    />,
+    { duration: 4000, unstyled: true }
+  );
+}
