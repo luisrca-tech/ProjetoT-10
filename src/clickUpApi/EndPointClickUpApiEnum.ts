fbddbb44b8fd9000d4bdcc20724233cb5129c6f6
P4vanzinho <thiago772000@gmail.com>
2024-08-01 11:17:43 -0300

    refactor : use zod in EndPointEnum

--- src/clickUpApi/EndPointClickUpApiEnum.ts
+++ src/clickUpApi/EndPointClickUpApiEnum.ts
@@ -1,4 +1,5 @@
-export enum EndPointClickUpApiEnum {
-  FIELD = "field",
-  TASK = "task",
-}
+import { z } from "zod";
+
+export const EndPointClickUpApiEnum = z.enum(["field", "task"]);
+
+export type EndPointClickUpApiEnum = z.infer<typeof EndPointClickUpApiEnum>;
