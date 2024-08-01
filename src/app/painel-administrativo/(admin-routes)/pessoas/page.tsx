984890ea76d2d21f45fd0ee2cc9f57e94005acb7
P4vanzinho <thiago772000@gmail.com>
2024-08-01 11:22:57 -0300

    cleanup: clean console.logs and imports

--- src/app/painel-administrativo/(admin-routes)/pessoas/page.tsx
+++ src/app/painel-administrativo/(admin-routes)/pessoas/page.tsx
@@ -3,13 +3,7 @@
 import { api } from "~/trpc/react";
 import { Container, MainContainer } from "./styles";
 
-const listId = "901303987731";
-
 export default function Pessoas() {
-  const getCustomFields = api.clickup.getCustomFields.useQuery({ listId });
-
-  console.log(getCustomFields.data);
-
   return (
     <Container>
       <MainContainer>
