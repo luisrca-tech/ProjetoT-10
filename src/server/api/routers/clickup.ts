2d1819a944be39d6f0354627c3faf0bf483ce950
P4vanzinho <thiago772000@gmail.com>
2024-08-01 11:15:48 -0300

    feat : add getTask trpc req

--- src/server/api/routers/clickup.ts
+++ src/server/api/routers/clickup.ts
@@ -1,46 +1,60 @@
 import { createTRPCRouter, publicProcedure } from "../trpc";
 import { z } from "zod";
+import { EndPointClickUpApiEnum } from "~/clickUpApi/EndPointClickUpApiEnum";
+import { type CustomField } from "~/app/types/clickUpApi";
+import { showToast } from "~/utils/functions/showToast";
+import { type Task } from "~/app/types/clickUpApi";
 
-type CustomField = {
-  id: string;
-  name: string;
-  type: string;
-  type_config: {
-    new_drop_down: boolean;
-    options: {
-      id: string;
-      name: string;
-      color: string | null;
-      orderindex: number;
-    };
-  };
-  date_created: string;
-  hide_from_guests: boolean;
-  required: boolean;
-  value: string[];
-};
+const listId = "901303987731";
 
 export const clickupRouter = createTRPCRouter({
   getCustomFields: publicProcedure
-    .input(z.object({ listId: z.string() }))
+    .input(z.object({ endPoint: EndPointClickUpApiEnum }))
     .query<CustomField[]>(async ({ input }) => {
       const response = await fetch(
-        `https://api.clickup.com/api/v2/list/${input.listId}/field`,
+        `https://api.clickup.com/api/v2/list/${listId}/${input.endPoint}`,
         {
           method: "GET",
           headers: {
-            "Content-Type": "string",
+            "Content-Type": "application/json",
             Authorization: "pk_81997206_S36OVHASAWZPBXJNMUNGQO4F1XJHEI8P",
           },
         }
       );
       const responseBody = await response.json();
       if (response.status === 401) {
-        console.log(`Erro :${responseBody.err}`);
-        console.log(`Erro :listId sem autorizacao.`);
-        return responseBody.err;
+        showToast(
+          "error",
+          "Erro de autorização ao acessar CustomFields!",
+          "Confira seus listId e AuthorizationToken"
+        );
+        return null;
       }
-
       return responseBody.fields;
+    }),
+
+  getTasks: publicProcedure
+    .input(z.object({ endPoint: EndPointClickUpApiEnum }))
+    .query<Task[]>(async ({ input }) => {
+      const response = await fetch(
+        `https://api.clickup.com/api/v2/list/${listId}/${input.endPoint}`,
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: "pk_81997206_S36OVHASAWZPBXJNMUNGQO4F1XJHEI8P",
+          },
+        }
+      );
+      const responseBody = await response.json();
+      if (response.status === 401) {
+        showToast(
+          "error",
+          "Erro de autorização ao acessar CustomFields!",
+          "Confira seus listId e AuthorizationToken"
+        );
+        return null;
+      }
+      return responseBody.tasks;
     }),
 });
