869b79cbd05d49c61102c3bf764513d43282fb16
P4vanzinho <thiago772000@gmail.com>
2024-08-13 14:13:05 -0300

    cleanup : clean styleds

--- src/components/surfaces/projectDetailsContent/styles.ts
+++ src/components/surfaces/projectDetailsContent/styles.ts
@@ -1,5 +1,4 @@
 import { styled } from "@linaria/react";
-import { theme } from "~/app/styles/theme";
 
 interface FormContainerProps {
   isDatePickerOpen: boolean;
@@ -24,7 +23,6 @@ export const MainContainer = styled.div`
   justify-content: center;
 `;
 
-// Container do formulário
 export const FormContainer = styled.div<FormContainerProps>`
   padding-top: ${(props) => (props.isProjectOptionsOpen ? "120px" : "0")};
   filter: ${(props) => (props.isDatePickerOpen ? `blur(2px)` : `none`)};
@@ -34,74 +32,4 @@ export const FormContainer = styled.div<FormContainerProps>`
   flex-direction: column;
   gap: 1rem;
   height: calc(100vh - 390px);
-`;
-
-// Container para os dados dos inputs
-export const InputsDataContainer = styled.div`
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  gap: 0.5rem;
-`;
-
-// Estilos para os inputs
-export const InputsData = styled.div`
-  width: 100%;
-  display: flex;
-  gap: 0.5rem;
-  height: 2.5rem;
-  align-items: center;
-
-  background-color: #f6f6f6;
-  border-radius: 60px;
-
-  span {
-    width: 100%;
-    font-size: 0.875rem;
-    color: ${theme.COLORS.SECONDARY_DARK};
-  }
-
-  .RoleSpacing {
-    padding: 0 0.5rem;
-  }
-`;
-
-// Estilos para o orçamento total
-export const TotalBudget = styled.footer`
-  display: flex;
-  justify-content: space-between;
-  margin-top: 1rem;
-
-  span,
-  strong {
-    font-family: "Poppins";
-  }
-
-  span {
-    font-weight: bold;
-    font-size: 0.875rem;
-  }
-`;
-
-// Container do switch
-export const SwitchContainer = styled.div`
-  width: 100%;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-
-  span {
-    font-size: 14px;
-    font-weight: 600;
-  }
-`;
-
-// Container para fechar o calendário
-export const CloseCalendarContainer = styled.div<FormContainerProps>`
-  width: 100%;
-  height: 100%;
-  position: fixed;
-  top: 0;
-  z-index: ${(props) => (props.isDatePickerOpen ? "1" : "-1")};
-  left: 0;
 `;
