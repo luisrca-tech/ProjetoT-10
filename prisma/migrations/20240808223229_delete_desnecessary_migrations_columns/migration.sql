f3dfb7665a8c14116ea45dd61f1bca4e9c4d709e
Luis Felipe <luisrochacruzalves@gmail.com>
2024-08-13 15:39:19 -0300

    ⬇️ Build: delete unsed columns in prisma

---
+++ prisma/migrations/20240808223229_delete_desnecessary_migrations_columns/migration.sql
@@ -0,0 +1,32 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `avatar_url` on the `users` table. All the data in the column will be lost.
+  - You are about to drop the column `email_verified` on the `users` table. All the data in the column will be lost.
+  - You are about to drop the column `name` on the `users` table. All the data in the column will be lost.
+  - You are about to drop the column `password` on the `users` table. All the data in the column will be lost.
+  - You are about to drop the `accounts` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `sessions` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `verificationtokens` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE "accounts" DROP CONSTRAINT "accounts_user_id_fkey";
+
+-- DropForeignKey
+ALTER TABLE "sessions" DROP CONSTRAINT "sessions_user_id_fkey";
+
+-- AlterTable
+ALTER TABLE "users" DROP COLUMN "avatar_url";
+ALTER TABLE "users" DROP COLUMN "email_verified";
+ALTER TABLE "users" DROP COLUMN "name";
+ALTER TABLE "users" DROP COLUMN "password";
+
+-- DropTable
+DROP TABLE "accounts";
+
+-- DropTable
+DROP TABLE "sessions";
+
+-- DropTable
+DROP TABLE "verificationtokens";
