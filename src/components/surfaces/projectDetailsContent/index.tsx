d4252c37a96d9f3d489718ce9b9b844bcf8c4eb0
P4vanzinho <thiago772000@gmail.com>
2024-08-13 14:13:42 -0300

    fix : use correct hook for reqs and add toast

--- src/components/surfaces/projectDetailsContent/index.tsx
+++ src/components/surfaces/projectDetailsContent/index.tsx
@@ -8,19 +8,35 @@ import { ProjectProfileHeader } from "../ProjectProfileHeader";
 import ToggleSwitch from "../../widgets/ToggleSwitch";
 import FormSelectInput from "../../forms/FormSelectInput";
 import { CloseCalendarContainer } from "./CloseCalendarContainer";
-
-import { useClickUp } from "~/app/hooks/useClickUp";
+import { useTasksOfProject } from "~/app/hooks/useTasksOfProject";
+import { useEffect } from "react";
+import { showToast } from "~/utils/functions/showToast";
+import { useRouter } from "next/navigation";
 
 export function ProjectDetailsContent() {
+  const router = useRouter();
   const [isDatePickerOpen] = useAtom(isDatePickerOpenAtom);
-  const { isFetchAllCustomFields } = useClickUp();
+  const { tasksOfProject, isFetchAllCustomFields, missingFields } =
+    useTasksOfProject();
+  const isDataFetched = !!tasksOfProject && !!isFetchAllCustomFields;
+
+  useEffect(() => {
+    if (missingFields) {
+      showToast(
+        "error",
+        "Erro ao carregar projeto",
+        `${missingFields} não existem na lista.`
+      );
+
+      router.push("/painel-administrativo/projetos");
+    }
+  }, [isDataFetched, missingFields, router]);
 
   return (
     <Container>
       {isFetchAllCustomFields && (
         <>
           <ProjectProfileHeader />
-
           <MainContainer>
             {isDatePickerOpen && <CustomDateRangePicker />}
             <FormContainer isDatePickerOpen={isDatePickerOpen}>
@@ -28,7 +44,6 @@ export function ProjectDetailsContent() {
               <FormSelectInput />
             </FormContainer>
           </MainContainer>
-
           <CloseCalendarContainer />
         </>
       )}
