92e6f11a550bb0164f1f04bdf1917868ff75ea0c
P4vanzinho <thiago772000@gmail.com>
2024-08-13 14:15:29 -0300

    fix : use correct hook  useFilteredTasksByProject and add toast

--- src/components/surfaces/project-card/index.tsx
+++ src/components/surfaces/project-card/index.tsx
@@ -1,53 +1,31 @@
 "use client";
 
-import {
-  CardContentDescriptions,
-  CardContentContainer,
-  Container,
-  ProjectContainer,
-  ProgressBarContainer,
-  BackgroundProgressBar,
-  ProgressBar,
-} from "./styles";
-import GoogleImage from "/public/images/google img.svg";
-
-import Image, { type StaticImageData } from "next/image";
-import { useEffect, useState } from "react";
+import { Container, ProjectContainer } from "./styles";
+import { CardContent } from "./CardContent";
 import { useRouter } from "next/navigation";
-import { poppins } from "~/assets/fonts/fonts";
-import { useClickUp } from "~/app/hooks/useClickUp";
+import { ProgressBar } from "./ProgressBar";
+import { useFilteredTasksByProject } from "~/app/hooks/useFilteredTasksByProject";
+import { useEffect } from "react";
+import { showToast } from "~/utils/functions/showToast";
 
 export function ProjectsCards() {
-  const [progressWidth, setProgressWidth] = useState(0);
   const router = useRouter();
-  const [passedMinutes, setPassedMinutes] = useState(0);
-
-  const totalHours = 3600000;
-
-  const { filteredTasksByProject } = useClickUp();
-  console.log(filteredTasksByProject, `filteredTasks`);
-
-  function formatDate(timestamp: number | null): string {
-    if (!timestamp) return "";
-    const date = new Date(timestamp);
-    return date.toLocaleDateString("pt-BR");
-  }
+  const { filteredTasksByProject, isNocustomFieldProject } =
+    useFilteredTasksByProject();
 
   function HandleClickProjectCard(projectId: string) {
     router.push(`/painel-administrativo/projeto?projectId=${projectId}`);
   }
 
   useEffect(() => {
-    const calculateProgress = () => {
-      setPassedMinutes((prevPassedMinutes) => prevPassedMinutes + 360000);
-      const progress = (passedMinutes / totalHours) * 100;
-      setProgressWidth(progress);
-    };
-
-    const intervalId = setInterval(calculateProgress, 3000);
-
-    return () => clearInterval(intervalId);
-  }, [passedMinutes, totalHours]);
+    if (!!isNocustomFieldProject) {
+      showToast(
+        "error",
+        "Não existe Projeto_PixelCraft na lista",
+        "Compare seu painel administrativo com a lista ClickUp!"
+      );
+    }
+  }, [isNocustomFieldProject]);
 
   return (
     <Container>
@@ -56,28 +34,9 @@ export function ProjectsCards() {
           key={project.id}
           onClick={() => HandleClickProjectCard(project.id)}
         >
-          <CardContentContainer>
-            <Image
-              src={GoogleImage as StaticImageData}
-              alt="Imagem logo da google"
-              color="#F7F2FA"
-            />
-            <CardContentDescriptions className={poppins.className}>
-              <strong>{project.label}</strong>
+          <CardContent project={project} dates={dates} />
 
-              <p>
-                <span>Duração</span>:{}
-                <span>{dates && formatDate(dates.minStartDate)}</span>
-                <span>-</span>
-                <span>{dates && formatDate(dates.maxEndDate)}</span>
-              </p>
-            </CardContentDescriptions>
-          </CardContentContainer>
-          <ProgressBarContainer>
-            <BackgroundProgressBar>
-              <ProgressBar style={{ width: `${progressWidth}%` }} />
-            </BackgroundProgressBar>
-          </ProgressBarContainer>
+          <ProgressBar />
         </ProjectContainer>
       ))}
     </Container>
