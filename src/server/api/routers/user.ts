701e0f0881e9866a14920146a126668f97c1bf57
Luis Felipe <luisrochacruzalves@gmail.com>
2024-08-13 15:45:22 -0300

    ✨ Feat: use trpc route to do validation and creaton in  my backend

--- src/server/api/routers/user.ts
+++ src/server/api/routers/user.ts
@@ -1,13 +1,34 @@
 import { z } from "zod";
+import { prisma } from "~/lib/prisma";
 import { createTRPCRouter, publicProcedure } from "../trpc";
 
 export const userRouter = createTRPCRouter({
+  checkUser: publicProcedure
+    .input(
+      z.object({
+        userId: z.string(),
+      })
+    )
+    .mutation(async ({ input }) => {
+      const dbUser = await prisma.user.findUnique({
+        where: { id: input.userId },
+      });
+
+      return { exists: !!dbUser };
+    }),
+
   create: publicProcedure
-    .input(z.object({ userId: z.string() }))
+    .input(
+      z.object({
+        userId: z.string(),
+        email: z.string().email(),
+      })
+    )
     .mutation(async ({ ctx, input }) => {
       await ctx.db.user.create({
         data: {
           id: input.userId,
+          email: input.email,
         },
       });
     }),
