3b807f28729d8c09eb4e2a22d98b443ca9fb48a4
Luis Felipe <luisrochacruzalves@gmail.com>
2024-08-13 15:47:42 -0300

    ✨ Feat: use creation from user in projetos page to create userId in db of oauth google user

--- src/app/painel-administrativo/(admin-routes)/projetos/page.tsx
+++ src/app/painel-administrativo/(admin-routes)/projetos/page.tsx
@@ -1,9 +1,25 @@
 "use client";
+
 import Header from "~/components/surfaces/header";
-import { Container } from "./styles";
 import { ProjectsCards } from "~/components/surfaces/project-card";
 
+import { useSession } from "@clerk/nextjs";
+import { useEffect, useState } from "react";
+import { useAuth } from "~/hooks/useAuth";
+import { Container } from "./styles";
+
 export default function Projetos() {
+  const { session } = useSession();
+  const { createOauthGoogleUser } = useAuth();
+  const [userCreated, setUserCreated] = useState(false);
+
+  useEffect(() => {
+    if (session?.user && !userCreated) {
+      createOauthGoogleUser();
+      setUserCreated(true);
+    }
+  }, [createOauthGoogleUser, session, userCreated]);
+  
   return (
     <Container>
       <Header />
