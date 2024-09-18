26435374b596449931e65c479938eb81324964c9
Luis Felipe <luisrochacruzalves@gmail.com>
2024-09-18 19:11:14 -0300

    ✨ Feat: create update req from tasks names

--- src/components/forms/FormForPeople/index.tsx
+++ src/components/forms/FormForPeople/index.tsx
@@ -5,6 +5,7 @@ import { loadingAtom } from "~/@atom/LoadingState/loadingAtom";
 import Button from "~/components/widgets/Button";
 import { useTasksOfProject } from "~/hooks/useTasksOfProject";
 import { formPersonsSchema } from "~/schemas/form-persons.schema";
+import { api } from "~/trpc/react";
 import { type formPersonsData } from "~/types/form-persons.type";
 import { showToast } from "~/utils/functions/showToast";
 import { FormFooter } from "../FormFooter";
@@ -12,9 +13,8 @@ import { Container, Form, PersonByRole, RoleAndPerson } from "./styles";
 
 export function FormForPeople() {
   const { getCustomFields } = useTasksOfProject();
-
   const roles = getCustomFields();
-
+  const updateTaskName = api.clickup.updateTaskName.useMutation();
   const [isLoading, setIsLoading] = useAtom(loadingAtom);
   const {
     register,
@@ -24,14 +24,20 @@ export function FormForPeople() {
     resolver: zodResolver(formPersonsSchema),
   });
 
-  const onSubmit = () => {
-    setTimeout(() => {
-      setIsLoading(true);
-    }, 1000);
-    setTimeout(() => {
-      setIsLoading(false);
+  const onSubmit = async ({ names }: formPersonsData) => {
+    setIsLoading(true);
+    try {
+      await updateTaskName.mutateAsync({
+        taskIds: roles?.map((role) => role.taskId),
+        names: names,
+      });
+
       showToast("success", "Pessoas alocadas com sucesso!");
-    }, 2500);
+    } catch (error) {
+      showToast("error", "Failed to update task name");
+    } finally {
+      setIsLoading(false);
+    }
   };
 
   return (
@@ -42,26 +48,26 @@ export function FormForPeople() {
       </RoleAndPerson>
       {roles ? (
         <Form onSubmit={handleSubmit(onSubmit)}>
-        {roles?.map((role, index) => (
-          <div key={index}>
-            <PersonByRole>
-              <input
-                type="text"
-                defaultValue={role.fieldName}
-                placeholder="Vincule uma pessoa..."
-                {...register(`names.${index}`)}
-              />
-              <span>{role.chargeName}</span>
-            </PersonByRole>
-            {errors.names && errors.names[index] && (
-              <p>{errors.names[index]?.message}</p>
-            )}
-          </div>
-        ))}
-        <FormFooter>
-          {!isLoading && <Button text="Salvar" type="submit" />}
-        </FormFooter>
-      </Form>
+          {roles?.map((role, index) => (
+            <div key={index}>
+              <PersonByRole>
+                <input
+                  type="text"
+                  defaultValue={role.fieldName}
+                  placeholder="Vincule uma pessoa..."
+                  {...register(`names.${index}`)}
+                />
+                <span>{role.chargeName}</span>
+              </PersonByRole>
+              {errors.names && errors.names[index] && (
+                <p>{errors.names[index]?.message}</p>
+              )}
+            </div>
+          ))}
+          <FormFooter>
+            {!isLoading && <Button text="Salvar" type="submit" />}
+          </FormFooter>
+        </Form>
       ) : (
         <span>Carregando...</span>
       )}
