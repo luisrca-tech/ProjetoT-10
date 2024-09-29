98a2b4f4436d47f4071919d515a70019cf427b75
P4vanzinho <thiago772000@gmail.com>
2024-09-29 07:38:51 -0300

    feat : create utils for apply clean code in components

---
+++ src/utils/functions/useGetTotalHoursAndValue.tsx
@@ -0,0 +1,32 @@
+import { useMemo } from "react";
+import { type TasksCustomFieldsType } from "~/server/types/Clickup.type";
+
+export const useGetTotalHoursAndValue = (
+  tasksCustomFields: TasksCustomFieldsType
+) => {
+  return useMemo(() => {
+    if (!tasksCustomFields) {
+      return { totalHours: 0, totalValue: 0 };
+    }
+
+    const { totalHours, totalValue } = tasksCustomFields.reduce(
+      (acc, task) => {
+        const hours = Array.isArray(task.hours)
+          ? task.hours.reduce((sum, h) => sum + parseFloat(h), 0)
+          : task.hours;
+
+        const valueByHour = Array.isArray(task.valueByHour)
+          ? task.valueByHour.reduce((prod, v) => prod * parseFloat(v), 1)
+          : task.valueByHour;
+
+        acc.totalHours += hours;
+        acc.totalValue += hours * valueByHour;
+
+        return acc;
+      },
+      { totalHours: 0, totalValue: 0 }
+    );
+
+    return { totalHours, totalValue };
+  }, [tasksCustomFields]);
+};
