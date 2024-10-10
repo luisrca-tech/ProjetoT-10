31e858ab5516999dd242ddc0fef1e353e6246127
P4vanzinho <thiago772000@gmail.com>
2024-10-10 16:40:37 -0300

    feat : create configuration page

---
+++ src/app/(admin-routes)/configuracao/page.tsx
@@ -0,0 +1,60 @@
+"use client";
+
+import { useSignIn } from "@clerk/nextjs";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { roboto } from "~/assets/fonts/fonts";
+import Input from "~/components/inputs/Input";
+import Button from "~/components/widgets/Button";
+import ErrorMessage from "~/components/widgets/ErrorMessage";
+
+import { Container, Form } from "./styles";
+import { type configurationType } from "~/types/configuration.type";
+import { configurationSchema } from "~/schemas/configuration-schema";
+
+export default function Configuration() {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors, isSubmitting },
+  } = useForm<configurationType>({
+    resolver: zodResolver(configurationSchema),
+    mode: "onChange",
+  });
+
+  // async function handleCreateKeys({ listId, pk }: loginType) {
+
+  //   // reset();
+  // }
+  const submitIsDisabled = !!errors.listId?.message || !!errors.pk?.message;
+  return (
+    <Container>
+      <Form>
+        <Input
+          id="listId"
+          type="text"
+          placeholder="List Id"
+          {...register("listId")}
+        />
+        <ErrorMessage>
+          {errors.listId?.message && errors.listId?.message}
+        </ErrorMessage>
+        <Input
+          id="pk"
+          type="text"
+          placeholder="Autorization Key"
+          {...register("pk")}
+        />
+        <ErrorMessage>{errors.pk?.message && errors.pk?.message}</ErrorMessage>
+        <Button
+          className={roboto.className}
+          loading={isSubmitting}
+          text="Entrar"
+          type="submit"
+          disabled={submitIsDisabled}
+        />
+      </Form>
+    </Container>
+  );
+}
---
+++ src/app/(admin-routes)/configuracao/styles.ts
@@ -0,0 +1,18 @@
+import { styled } from "@linaria/react";
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  height: 100%;
+  justify-content: center;
+  margin: auto;
+  width: 328px;
+`;
+
+export const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  width: 100%;
+`;
