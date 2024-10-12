ed6c1d1e10a96920b00953ea972d152778c3e250
Luis Felipe <luisrochacruzalves@gmail.com>
2024-10-12 15:30:01 -0300

    ♻️ Refactor: improve validation of without keys

--- src/components/surfaces/project-card/index.tsx
+++ src/components/surfaces/project-card/index.tsx
@@ -1,32 +1,52 @@
 "use client";
 
+import { useSession } from "@clerk/nextjs";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 import { Skeleton } from "~/components/widgets/Skeleton";
 import { useFilteredTasksByProject } from "~/hooks/useFilteredTasksByProject";
+import { api } from "~/trpc/react";
 import { showToast } from "~/utils/functions/showToast";
 import { CardContent } from "./CardContent";
 import { ProgressBar } from "./ProgressBar";
 import { Container, ProjectContainer } from "./styles";
 
 export function ProjectsCards() {
+  const { session } = useSession();
+  const userId = session?.user.id;
   const router = useRouter();
-  const { filteredTasksByProject, isNocustomFieldProject } =
-    useFilteredTasksByProject();
+  const { filteredTasksByProject } = useFilteredTasksByProject();
 
   function HandleClickProjectCard(projectId: string) {
     router.push(`/espelho?projectId=${projectId}`);
   }
 
+  const getClickupKeys = api.clickup.getClickupKeys.useQuery({
+    userId: userId ?? "",
+  });
+
   useEffect(() => {
-    if (!!isNocustomFieldProject) {
-      showToast(
-        "error",
-        "Não existe Projeto_PixelCraft na lista",
-        "Confira sua configuração"
-      );
+    const timeoutId = setTimeout(() => {
+      if (
+        !getClickupKeys.data?.AuthorizationPkKey &&
+        !getClickupKeys.data?.listId
+      ) {
+        showToast(
+          "error",
+          "Não encontramos PK ou ListId cadastrados",
+        );
+      }
+      router.push("/configuracao");
+    }, 4000);
+
+    if (
+      getClickupKeys.data?.AuthorizationPkKey &&
+      getClickupKeys.data?.listId
+    ) {
+      clearTimeout(timeoutId);
     }
-  }, [isNocustomFieldProject]);
+    return () => clearTimeout(timeoutId);
+  }, [getClickupKeys, router]);
 
   return (
     <Container>
