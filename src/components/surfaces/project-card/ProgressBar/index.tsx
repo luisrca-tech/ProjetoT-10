b394573a40c3f772607b16b2b2747d45396207e6
P4vanzinho <thiago772000@gmail.com>
2024-10-16 15:45:12 -0300

    feat : create ProgressBar with real dates

--- src/components/surfaces/project-card/ProgressBar/index.tsx
+++ src/components/surfaces/project-card/ProgressBar/index.tsx
@@ -1,29 +1,56 @@
 "use client";
 import { useEffect, useState } from "react";
 import { Container, BackgroundProgressBar, ProgressBarContent } from "./styles";
+import { useTasksOfProject } from "~/hooks/useTasksOfProject";
+import { useGetBudgetAndProfileInfos } from "~/utils/functions/useGetBudgetAndProfileInfos";
 
-export function ProgressBar() {
+type ProgressBarProps = {
+  project: {
+    id?: string;
+    name?: string;
+    color?: string | null;
+    orderindex?: number;
+    label?: string;
+  };
+};
+export function ProgressBar({ project }: ProgressBarProps) {
   const [progressWidth, setProgressWidth] = useState(0);
-  const [passedMinutes, setPassedMinutes] = useState(0);
+  const projectId = project.id;
+  const { getTasksInfos } = useTasksOfProject(projectId);
+  const tasksCustomFields = getTasksInfos();
 
-  const totalHours = 3600000;
+  const { maxEndDateObj, minStartDateObj } =
+    useGetBudgetAndProfileInfos(tasksCustomFields);
 
   useEffect(() => {
+    if (!minStartDateObj || !maxEndDateObj) {
+      return;
+    }
+
+    const totalDuration = maxEndDateObj.getTime() - minStartDateObj.getTime();
+
     const calculateProgress = () => {
-      setPassedMinutes((prevPassedMinutes) => prevPassedMinutes + 360000);
-      const progress = (passedMinutes / totalHours) * 100;
+      const now = Date.now();
+      const elapsedTime = now - minStartDateObj.getTime();
+
+      const clampedElapsedTime = Math.max(elapsedTime, 0);
+      const progress = Math.min(
+        (clampedElapsedTime / totalDuration) * 100,
+        100
+      );
       setProgressWidth(progress);
     };
 
-    const intervalId = setInterval(calculateProgress, 3000);
+    calculateProgress();
+  }, [minStartDateObj, maxEndDateObj]);
 
-    return () => clearInterval(intervalId);
-  }, [passedMinutes, totalHours]);
   return (
     <Container>
-      <BackgroundProgressBar>
-        <ProgressBarContent style={{ width: `${progressWidth}%` }} />
-      </BackgroundProgressBar>
+      {tasksCustomFields && (
+        <BackgroundProgressBar>
+          <ProgressBarContent style={{ width: `${progressWidth}%` }} />
+        </BackgroundProgressBar>
+      )}
     </Container>
   );
 }
