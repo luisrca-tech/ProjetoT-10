a44ba9f3b093ff1e9b7ea76ba7550156a31c83e8
P4vanzinho <thiago772000@gmail.com>
2024-10-21 11:42:01 -0300

    refactor : change spacings of pages

--- src/components/surfaces/projectDetailsContent/styles.ts
+++ src/components/surfaces/projectDetailsContent/styles.ts
@@ -12,7 +12,7 @@ export const Container = styled.div`
 `;
 
 export const MainContainer = styled.div`
-  margin-top: 13.594rem;
+  margin-top: 12.5rem;
   padding: 0 5%;
   width: 100%;
   height: 100%;
@@ -31,7 +31,7 @@ export const FormContainer = styled.div<FormContainerProps>`
   display: flex;
   flex-direction: column;
   gap: 1rem;
-  height: calc(100vh - 390px);
+  height: calc(100vh - 430px);
 `;
 
 export const InputsContent = styled.div`
